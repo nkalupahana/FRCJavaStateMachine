3e5450cc19f46ef91c5ab95e0cbfce69c40543a4
nkalupahana <avishkank@gmail.com>
2024-04-30 23:32:31 -0500

    Remove duplicate controller

--- src/main/java/frc/robot/subsystems/manager/Manager.java
+++ src/main/java/frc/robot/subsystems/manager/Manager.java
@@ -1,13 +1,11 @@
 package frc.robot.subsystems.manager;
 
-import edu.wpi.first.wpilibj.XboxController;
 import frc.robot.subsystems.Subsystem;
 import frc.robot.subsystems.intake.Intake;
 import frc.robot.Robot;
 
 public class Manager extends Subsystem<ManagerStates> {
     Intake intake = new Intake();
-    XboxController controller = new XboxController(0);
 
     public Manager() {
         super("Manager", ManagerStates.IDLE);
